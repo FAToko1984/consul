8b1aa6b1965205aeef45252fbafe787daccec15b
itw-smart <tobias@itw-smartbusiness.de>
2020-08-25 08:28:50 +0200

    census

--- spec/models/custom/verification/residence_spec.rb
+++ spec/models/custom/verification/residence_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe Verification::Residence do
-  let(:residence) { build(:verification_residence, document_number: "12345678Z") }
+  let(:residence) { build(:verification_residence, document_number: "LF083L1J1") }
 
   describe "verification" do
     describe "postal code" do
@@ -15,7 +15,7 @@ describe Verification::Residence do
         expect(residence.errors[:postal_code]).to be_empty
       end
 
-      it "is not valid with postal codes not starting with 280" do
+      it "is not valid with postal codes not starting with 8" do
         residence.postal_code = "12345"
         residence.valid?
         expect(residence.errors[:postal_code].size).to eq(1)
